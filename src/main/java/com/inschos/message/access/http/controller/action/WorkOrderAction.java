69f5ea34200080fd5bdb51ea4142f2e4fa55b0ab
meiming <meiming@bigwei.com>
2018-05-22 14:58:58 +0800

    ok

--- src/main/java/com/inschos/message/access/http/controller/action/WorkOrderAction.java
+++ src/main/java/com/inschos/message/access/http/controller/action/WorkOrderAction.java
@@ -319,6 +319,8 @@ public class WorkOrderAction extends BaseAction {
                     ChannelBean channel = channelClient.getChannel(String.valueOf(agent.channel_id));
                     if (channel != null) {
                         data.channelName = channel.name;
+                    }else{
+                        data.channelName = "";
                     }
                 }
                 data.senderName = agent.name;
