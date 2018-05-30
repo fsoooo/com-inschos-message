30700abedbc48f70a0045434606dbe8b0fa688ec
wangshilei <wangsl@inschos.com>
2018-05-30 21:29:19 +0800

    '05.30'

--- src/main/java/com/inschos/message/access/http/controller/bean/MsgSendResBean.java
+++ src/main/java/com/inschos/message/access/http/controller/bean/MsgSendResBean.java
@@ -6,7 +6,11 @@ public class MsgSendResBean {
 
     public int toType;//'收件人类型，，个人1/企业2/代理人3/业管4
 
+    public String toName = "收件人姓名";//'收件人类型，，个人1/企业2/代理人3/业管4
+
     public long channelId;//渠道
+
+    public String channelName = "渠道姓名";//渠道
 
     public String sendRes;//发送结果
 }
